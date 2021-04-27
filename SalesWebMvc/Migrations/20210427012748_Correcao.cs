ecec5f4b2c9e3bccb04ecc4c66bbd9bce6d6cb51
Gustavo Zotarelli <gustavozotarelli@gmail.com>
2021-04-26 22:35:02 -0300

    Arrumando Migration do banco, implementada a tela de Sellers utilizando FindAll

---
+++ SalesWebMvc/Migrations/20210427012748_Correcao.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class Correcao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Seller",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Seller",
+                newName: "Nome");
+        }
+    }
+}
