720fc8a29f8170bf81615a09e00b7b79162e81aa
Gustavo Zotarelli <gustavozotarelli@gmail.com>
2021-04-27 23:31:09 -0300

    Configurando os métodos de Alteração, Consulta e Exclusão de vendedor

--- SalesWebMvc/Models/Services/SellerService.cs
+++ SalesWebMvc/Models/Services/SellerService.cs
@@ -1,4 +1,6 @@
-﻿using SalesWebMvc.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models.Services.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +20,41 @@ namespace SalesWebMvc.Models.Services
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj) 
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if(!(_context.Seller.Any(x => x.Id == obj.Id)))
+            {
+                throw new NotFoundException("Id not Found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
